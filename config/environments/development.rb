cc5d436e44f5315682dce6c1fb697fdd3c920281
florent sandri <sandri822@hotmail.fr>
2022-11-22 18:13:58 +0100

    🚀 Deploy stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
