fd6d5e35cc8f29796c490cbc8a4d02b3faf2e4b7
florent sandri <sandri822@hotmail.fr>
2022-11-23 14:37:19 +0100

    ✅ Add username and Admin

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 5 , maximum: 100 }
   validates :subtitle, length: { minimum: 5 , maximum: 100 }
-  validates :description, presence: true, length: { minimum: 50 }
+  validates :description, presence: true, length: { minimum: 5 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
+
   root "articles#index"
 
   resources :contacts, only: [:new, :create ]
