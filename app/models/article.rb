186e86e8e0e8a086c64f72e3d9cad1a66e3ee436
florent sandri <sandri822@hotmail.fr>
2022-11-24 09:48:30 +0100

    💄 Style message and add role admin

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 5 , maximum: 100 }
   validates :subtitle, length: { minimum: 5 , maximum: 100 }
-  validates :description, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 , maximum: 3500 }
 
   belongs_to :user
 end
