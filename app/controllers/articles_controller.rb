68297b0259577bc5e1ff1172431be829d1b475f6
florent sandri <sandri822@hotmail.fr>
2022-11-25 19:58:22 +0100

    💄 Add picture

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,8 +56,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def post; end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
