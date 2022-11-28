3cc49690d93cbab5a35ad960ee043179081c263f
florent sandri <sandri822@hotmail.fr>
2022-11-28 18:12:50 +0100

    🗃️ Added host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
     enable_starttls_auto: true }
 
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << ENV['HOSTNAME']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
