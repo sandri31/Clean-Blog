d1544a01766280f4e3baa6e3f7f653bbdd687e0e
Symitsh <sandri822@hotmail.fr>
2023-01-23 09:28:19 +0100

    🔧 Update config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.hosts << ENV['HOSTNAME']
+
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
