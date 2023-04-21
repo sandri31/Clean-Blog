182fad86b42f6161a7af9f9c8f2419e8c1c4ee83
Florent S <sandri822@hotmail.fr>
2023-04-21 14:06:53 +0200

    🔧 Update config

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
-  validates :body, presence: true, length: { minimum: 5, maximum: 10000 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 10_000 }
   validates :user_id, presence: true
 
   extend FriendlyId
