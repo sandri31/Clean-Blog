6996a316a9447f7d995b19dd38ea459e6c8e4da6
Florent S <sandri822@hotmail.fr>
2023-06-25 21:11:48 +0200

    🚧 test controllers

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :article do
     title { 'Test Article' }
-    body { 'This is a test article.' }
+    body { "This is a valid body with more than five characters." }
     association :user
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # Include Devise helpers for controller specs
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
