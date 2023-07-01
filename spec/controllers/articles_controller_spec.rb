27dc28915ae261d45177d1aebc1f2afa8e26df23
Florent S <sandri822@hotmail.fr>
2023-07-01 17:56:08 +0200

    ✅ Add tests for login requirement

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -31,4 +31,50 @@ RSpec.describe ArticlesController, type: :controller do
       expect(assigns(:article)).to eq(@article)
     end
   end
+
+  let(:valid_attributes) do
+    { title: 'Test Article', body: 'This is a test article.' }
+  end
+
+  let(:invalid_attributes) do
+    { title: nil, body: nil }
+  end
+
+  describe 'GET #new' do
+    it 'requires login' do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'requires login' do
+      post :create, params: { article: valid_attributes }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'requires login' do
+      article = FactoryBot.create(:article)
+      get :edit, params: { id: article.friendly_id }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'PUT #update' do
+    it 'requires login' do
+      article = FactoryBot.create(:article)
+      put :update, params: { id: article.friendly_id, article: valid_attributes }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'requires login' do
+      article = FactoryBot.create(:article)
+      delete :destroy, params: { id: article.friendly_id }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
