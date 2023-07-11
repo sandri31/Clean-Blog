fe7a71647c75e67ad93997af5ad68e939802feb2
Florent S <sandri822@hotmail.fr>
2023-07-11 11:38:02 +0200

    🧪 Write tests for Subscriber model

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    subscriber = Subscriber.new(email: 'test@example.com')
+    expect(subscriber).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    subscriber = Subscriber.new(email: nil)
+    expect(subscriber).to_not be_valid
+  end
+
+  it 'is not valid with a duplicate email' do
+    Subscriber.create!(email: 'test@example.com')
+    subscriber = Subscriber.new(email: 'test@example.com')
+    expect(subscriber).to_not be_valid
+  end
 end
