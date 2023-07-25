c35b22cf8165743b86ad05624d2f5efa6ee2fe5f
Florent S <sandri822@hotmail.fr>
2023-07-25 11:10:12 +0200

    ➕ Add dependence i18n and update style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def gravatar_for(email, size: 120)
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: email, class: 'gravatar rounded-circle')
+  end
 end
